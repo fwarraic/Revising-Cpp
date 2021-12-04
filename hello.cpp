ded59ed6055af695b0d3a087afefb32cd7d634b8
Fazal <fwarraic@sfu.ca>
2021-12-03 23:34:47 -0800

    First commit

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Hello World" << endl;
+    cout << "Yes its the output" << endl;
+
+//    int *pHeight = nullptr;
+//    cout << *pHeight << endl;
+
+    return 0;
+}
