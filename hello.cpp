6c0ecd90c91690ec00ae9dcd2639d49c7f4617c6
Fazal <fwarraic@sfu.ca>
2021-12-04 00:13:26 -0800

    All set!

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,14 @@ int main(){
     cout << "Hello World" << endl;
     cout << "Yes its the output" << endl;
 
+    cout << "VS Code Check" << endl;
+    cout << "Git Check" << endl;
+
+    cout << "Compile" << endl;
+    cout << "Run" << endl;
+    cout << "Publish on github" << endl;
+    cout << "Success" << endl;
+
 //    int *pHeight = nullptr;
 //    cout << *pHeight << endl;
 
