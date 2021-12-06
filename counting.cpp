b92c7af93cbb3c4484e4bf9781fdaa1023213529
Fazal <fwarraic@sfu.ca>
2021-12-05 19:11:26 -0800

    Counting till 100, revisiting for loops now.

---
+++ counting.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Today we will count to 100 :)" << endl;
+
+    cout << endl;
+
+    for (int i = 1; i <= 100; i++){
+        cout << i << endl;
+    }
+
+    cout << endl;
+
+    cout << "Done!" << endl; 
+    
+
+}
