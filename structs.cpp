f38ba964a438fcb3658ec540269d53013386e2dd
Fazal <fwarraic@sfu.ca>
2021-12-09 21:25:57 -0800

    Know how to use structs now

--- structs.cpp
+++ structs.cpp
@@ -10,6 +10,8 @@ struct Employee{
 
 };
 
+void printEmployeeInfo(struct Employee employee);
+
 
 int main(){
     cout << "Hello Structs!" << endl;
@@ -40,7 +42,28 @@ int main(){
     cout << employee1.name << " " << employee1.phoneNumber << endl;
 
     cout << endl;
+    
+    cout << "----------------" << endl;
+    cout << "Printing Employee Info for Employee1: " << endl;
+    printEmployeeInfo(employee1);
+    cout << endl;
+
+    cout << "----------------" << endl;
+    cout << "Printing Employee Info for Employee2: " << endl;
+    printEmployeeInfo(employee2);
+    cout << endl;
+
+    cout << "----------------" << endl;
+    cout << "Printing Employee Info for Employee3: " << endl;
+    printEmployeeInfo(employee3);
+    cout << endl;
 
 
 
+}
+
+void printEmployeeInfo(struct Employee employee){
+    cout << "Name: " << employee.name << endl;
+    cout << "Phone Number: " << employee.phoneNumber << endl;
+    cout << "employee Number: " << employee.employeeNumber << endl;
 }
